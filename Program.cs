53f234efc045b8f13b4d15d6341df858a88d7747
Grecas <greisytoo@gmail.com>
2019-10-13 23:55:25 -0300

    Se permite leer juegos de datos desde archivo externo. Se incluye archivo Data.txt con juego de datos

--- Program.cs
+++ Program.cs
@@ -9,28 +9,24 @@ namespace TrafficBr
         
         static void Main(string[] args)
         {
-            List<Semaphore> s = new List<Semaphore>()
-            {
-                new Semaphore { X = 1, R = 2, G = 3 },
-                new Semaphore { X = 6, R = 2, G = 3 },
-                new Semaphore { X = 10, R = 2, G = 3 },
-                new Semaphore { X = 16, R = 3, G = 4 }
-            };
-           
+            List<Semaphore> s = new List<Semaphore>();
+
             //// Read each line of the file into a string array. 
-            //string[] lines = System.IO.File.ReadAllLines(@"C:\Data.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"Data.txt");
 
-            //// Display the file contents by using a foreach loop.
-            //foreach (string line in lines)
-            //{
-                
-            //}
-                      
+            // Read and create the semaphores properties
+            for(int line = 1; line< lines.Length; line++)
+            {
+                string[] properties = lines[line].Split(" ");
+                s.Add(new Semaphore { X = int.Parse(properties[0]), R = int.Parse(properties[1]), G = int.Parse(properties[2])});
+            }
 
-
+            //Le calcula a cada semaphore la probabilidad de ser el primero en rojo, para un 0<=t<=TotalCiclos
             List<float> a = TrafficBlights.pBeTheFirst(s);
             foreach (float fl in a)
                 Console.WriteLine("Semaphore: " + fl);
+
+            //Calcula la probabilidad de que todos los semaphore esten en verde para cada intervalo de tiempo
             var pGreen = TrafficBlights.pAllInGreen();
             Console.WriteLine("All in Green: " + pGreen);
 
